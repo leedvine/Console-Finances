b567d69e286f11c16ce66a4b5219a98382929b71
Lee Vine <leedvine@gmail.com>
2022-12-19 15:54:17 +0000

    Calculated Total profit and loss, total months, and the average change over full period

--- index.js
+++ index.js
@@ -3,153 +3,196 @@
 // and number (numbers)
 
 let finances = [
-['Jan-2010', 867884],
-['Feb-2010', 984655],
-['Mar-2010', 322013],
-['Apr-2010', -69417],
-['May-2010', 310503],
-['Jun-2010', 522857],
-['Jul-2010', 1033096],
-['Aug-2010', 604885],
-['Sep-2010', -216386],
-['Oct-2010', 477532],
-['Nov-2010', 893810],
-['Dec-2010', -80353],
-['Jan-2011', 779806],
-['Feb-2011', -335203],
-['Mar-2011', 697845],
-['Apr-2011', 793163],
-['May-2011', 485070],
-['Jun-2011', 584122],
-['Jul-2011', 62729],
-['Aug-2011', 668179],
-['Sep-2011', 899906],
-['Oct-2011', 834719],
-['Nov-2011', 132003],
-['Dec-2011', 309978],
-['Jan-2012', -755566],
-['Feb-2012', 1170593],
-['Mar-2012', 252788],
-['Apr-2012', 1151518],
-['May-2012', 817256],
-['Jun-2012', 570757],
-['Jul-2012', 506702],
-['Aug-2012', -1022534],
-['Sep-2012', 475062],
-['Oct-2012', 779976],
-['Nov-2012', 144175],
-['Dec-2012', 542494],
-['Jan-2013', 359333],
-['Feb-2013', 321469],
-['Mar-2013', 67780],
-['Apr-2013', 471435],
-['May-2013', 565603],
-['Jun-2013', 872480],
-['Jul-2013', 789480],
-['Aug-2013', 999942],
-['Sep-2013', -1196225],
-['Oct-2013', 268997],
-['Nov-2013', -687986],
-['Dec-2013', 1150461],
-['Jan-2014', 682458],
-['Feb-2014', 617856],
-['Mar-2014', 824098],
-['Apr-2014', 581943],
-['May-2014', 132864],
-['Jun-2014', 448062],
-['Jul-2014', 689161],
-['Aug-2014', 800701],
-['Sep-2014', 1166643],
-['Oct-2014', 947333],
-['Nov-2014', 578668],
-['Dec-2014', 988505],
-['Jan-2015', 1139715],
-['Feb-2015', 1029471],
-['Mar-2015', 687533],
-['Apr-2015', -524626],
-['May-2015', 158620],
-['Jun-2015', 87795],
-['Jul-2015', 423389],
-['Aug-2015', 840723],
-['Sep-2015', 568529],
-['Oct-2015', 332067],
-['Nov-2015', 989499],
-['Dec-2015', 778237],
-['Jan-2016', 650000],
-['Feb-2016', -1100387],
-['Mar-2016', -174946],
-['Apr-2016', 757143],
-['May-2016', 445709],
-['Jun-2016', 712961],
-['Jul-2016', -1163797],
-['Aug-2016', 569899],
-['Sep-2016', 768450],
-['Oct-2016', 102685],
-['Nov-2016', 795914],
-['Dec-2016', 60988],
-['Jan-2017', 138230],
-['Feb-2017', 671099]
-];
-
-
-// Create two variables that 
-// can eventually hold the
-// strings and numbers seperately
-
-let monthsCombined = []
-let numbersCombined = []
-
-// Create a for loop to seperate string
-//  from numbers naming them date and 
-// numbers
-
-for (let i = 0; i < finances.length; i++) {
-    const [date, numbers ] = finances[i];
-
-//Console log to test seperation of date
-// and numbers - if successful will be on 
-// seperate lines
-    console.log(date);
-    console.log(numbers);
-
-
-    // Push all the dates in to the same array
-
-    monthsCombined.push(date);
-
- // Push all the numbers in to the same array
- 
- numbersCombined.push(numbers);
-
-   
-}
-
- // Test to see if all in one array
-
- console.log(monthsCombined);
-
-  // Test to see if all numbers in one array
-  console.log(numbersCombined);
-
-  //Create a variable to count total months
-//   and console log answer
-
-let totalMonths = monthsCombined.length;
-
-console.log("Total Months: " + totalMonths);
-
-  //Create a variable that will add up all
-//    the numbers (subtracting where req'd)
-//   and show result in console
-
-const sum = numbersCombined.reduce((sumCounter, value) => {
-    return sumCounter + value;
-    }, 0);
-
-    console.log("Total: $" + sum); 
+    ['Jan-2010', 867884],
+    ['Feb-2010', 984655],
+    ['Mar-2010', 322013],
+    ['Apr-2010', -69417],
+    ['May-2010', 310503],
+    ['Jun-2010', 522857],
+    ['Jul-2010', 1033096],
+    ['Aug-2010', 604885],
+    ['Sep-2010', -216386],
+    ['Oct-2010', 477532],
+    ['Nov-2010', 893810],
+    ['Dec-2010', -80353],
+    ['Jan-2011', 779806],
+    ['Feb-2011', -335203],
+    ['Mar-2011', 697845],
+    ['Apr-2011', 793163],
+    ['May-2011', 485070],
+    ['Jun-2011', 584122],
+    ['Jul-2011', 62729],
+    ['Aug-2011', 668179],
+    ['Sep-2011', 899906],
+    ['Oct-2011', 834719],
+    ['Nov-2011', 132003],
+    ['Dec-2011', 309978],
+    ['Jan-2012', -755566],
+    ['Feb-2012', 1170593],
+    ['Mar-2012', 252788],
+    ['Apr-2012', 1151518],
+    ['May-2012', 817256],
+    ['Jun-2012', 570757],
+    ['Jul-2012', 506702],
+    ['Aug-2012', -1022534],
+    ['Sep-2012', 475062],
+    ['Oct-2012', 779976],
+    ['Nov-2012', 144175],
+    ['Dec-2012', 542494],
+    ['Jan-2013', 359333],
+    ['Feb-2013', 321469],
+    ['Mar-2013', 67780],
+    ['Apr-2013', 471435],
+    ['May-2013', 565603],
+    ['Jun-2013', 872480],
+    ['Jul-2013', 789480],
+    ['Aug-2013', 999942],
+    ['Sep-2013', -1196225],
+    ['Oct-2013', 268997],
+    ['Nov-2013', -687986],
+    ['Dec-2013', 1150461],
+    ['Jan-2014', 682458],
+    ['Feb-2014', 617856],
+    ['Mar-2014', 824098],
+    ['Apr-2014', 581943],
+    ['May-2014', 132864],
+    ['Jun-2014', 448062],
+    ['Jul-2014', 689161],
+    ['Aug-2014', 800701],
+    ['Sep-2014', 1166643],
+    ['Oct-2014', 947333],
+    ['Nov-2014', 578668],
+    ['Dec-2014', 988505],
+    ['Jan-2015', 1139715],
+    ['Feb-2015', 1029471],
+    ['Mar-2015', 687533],
+    ['Apr-2015', -524626],
+    ['May-2015', 158620],
+    ['Jun-2015', 87795],
+    ['Jul-2015', 423389],
+    ['Aug-2015', 840723],
+    ['Sep-2015', 568529],
+    ['Oct-2015', 332067],
+    ['Nov-2015', 989499],
+    ['Dec-2015', 778237],
+    ['Jan-2016', 650000],
+    ['Feb-2016', -1100387],
+    ['Mar-2016', -174946],
+    ['Apr-2016', 757143],
+    ['May-2016', 445709],
+    ['Jun-2016', 712961],
+    ['Jul-2016', -1163797],
+    ['Aug-2016', 569899],
+    ['Sep-2016', 768450],
+    ['Oct-2016', 102685],
+    ['Nov-2016', 795914],
+    ['Dec-2016', 60988],
+    ['Jan-2017', 138230],
+    ['Feb-2017', 671099]
+    ];
     
     
-    //display results in browser
-
-    document.getElementById("totalMonths").innerHTML = totalMonths;
-    document.getElementById("totalAmount").innerHTML = sum;
+    // Create two variables that 
+    // can eventually hold the
+    // strings and numbers seperately
+    
+    let monthsCombined = []
+    let amountCombined = []
+    
+    // Create a for loop to seperate string
+    //  from numbers naming them date and 
+    // numbers
+    
+    for (let i = 0; i < finances.length; i++) {
+        const [date, numbers ] = finances[i];
+    
+    //Console log to test seperation of date
+    // and numbers - if successful will be on 
+    // seperate lines
+        console.log(date);
+        console.log(numbers);
+    
+    
+        // Push all the dates in to the same array
+    
+        monthsCombined.push(date);
+    
+     // Push all the numbers in to the same array
+     
+     amountCombined.push(numbers);
+    
+       
+    }
+    
+     // Test to see if all in one array
+    
+     console.log(monthsCombined);
+    
+      // Test to see if all numbers in one array
+      console.log(amountCombined);
+    
+      //Create a variable to count total months
+    //   and console log answer
+    
+    const totalMonths = monthsCombined.length;
+    
+    console.log("Total Months: " + totalMonths);
+    
+      //Create a variable to store all the numbers (subtracting where req'd)
+    //   and show result in console
+    
+    let sum = 0;
+    
+    // Running the for loop to add numbers
+    for (let i = 0; i < amountCombined.length; i++) {
+        sum += amountCombined[i];
+    }
+    
+        console.log("Total: $" + sum); 
+    
+        //Use .map to find difference in amounts month to month
+        
+        let numberDiff = amountCombined.slice(1).map((x,i)=> x-amountCombined[i]);
+    
+        console.log(numberDiff);
+    
+        //Get total change for the full period
+    
+        let totalChange = 0;
+    
+        // Running the for loop to add numbers
+        for (let i = 0; i < numberDiff.length; i++) {
+            totalChange += numberDiff[i];
+        }
+    
+        console.log(totalChange);
+    
+     //Find average change by dividing then create variable to round to two decimal places
+    
+        const totalMonthsCalc = totalMonths-1;
+    
+        console.log("Lee" + totalMonthsCalc);
+    
+        const averageChange = totalChange / totalMonthsCalc;
+    
+        const averageChangeRounded = averageChange.toFixed(2);
+    
+        console.log("Average Change: $ " + averageChangeRounded);
+    
+    
+    
+    
+        //Find biggest number
+    
+    
+    
+    
+    
+        
+        
+        //display results in browser
+    
+        document.getElementById("totalMonths").innerHTML = totalMonths;
+        document.getElementById("totalAmount").innerHTML = sum;
+    
+    
